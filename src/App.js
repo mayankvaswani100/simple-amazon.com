f9a0767d55349e7e42bccc7ccd97d456e1cb519c
Programming is a Joke <coder.mayan100@Gmail.com>
2022-07-20 22:36:42 +0530

    Integrated Tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import "./App.css";
 import React from "react";
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div>
       <h1 className="title">Simple Amazon.com</h1>
       <div className="banner-img"></div>
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Card.css";
+
+function Card() {
+  return (
+    <div className="card">
+      <h1>h</h1>
+    </div>
+  );
+}
+
+export default Card;
